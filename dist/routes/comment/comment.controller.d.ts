b9e4e6e10f7ab636c12427bd0dcffaa98e57f867
Christian Grimm <grimmchristian@gmx.de>
2026-03-12 09:56:20 +0100

    Remove node_modules and dist from repository

--- dist/routes/comment/comment.controller.d.ts
+++
@@ -1,3 +0,0 @@
-declare const commentRouter: import("express-serve-static-core").Router;
-export default commentRouter;
-//# sourceMappingURL=comment.controller.d.ts.map
--- dist/user/user.service.d.ts
+++
@@ -1,4 +0,0 @@
-import { User } from "./user.type.";
-export declare function getUserById(id: number): User;
-export declare function getCommentsByUserId(userId: number, skip?: number, take?: number): import("./user.type.").Comment[];
-//# sourceMappingURL=user.service.d.ts.map
